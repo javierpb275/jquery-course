c401fc6b012d4681b593a56fba60fd30694ba42b
javierpb275 <javierpb275@gmail.com>
2022-02-09 19:14:38 +0100

    animated scroll to a section

--- lesson7/main.js
+++ lesson7/main.js
@@ -1,25 +1,20 @@
 $(document).ready(() => {
-  $(".content-box").click(function () {
-    $(".content-box")
-      .animate(
-        {
-          width: "500px",
-          height: "400px",
-        },
-        3000
-      )
-      .animate(
-        {
-          fontSize: "30px",
-        },
-        3000
-      )
-      .fadeOut(3000);
-
-    $("button").click(function () {
-      $(".content-box").stop(true, true);//makes the animation happen instantly
-      //stop(): stop animation running
-      //stop(true): stop all animations
-    });
-  });
+  const page_url = window.location.href;
+  const page_id = page_url.substring(page_url.lastIndexOf("#") + 1);
+  console.log(page_id);
+  if (page_id == "section3") {
+    $("html, body").animate(
+      {
+        scrollTop: $("#scroll-" + page_id).offset().top,
+      },
+      1000
+    );
+  } else if (page_id == "post") {
+    $("html, body").animate(
+      {
+        scrollTop: $("#scroll-" + page_id).offset().top,
+      },
+      1000
+    );
+  }
 });
