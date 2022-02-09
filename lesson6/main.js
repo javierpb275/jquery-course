7502c0e221e803444225c677f189fe33af2dfd19
javierpb275 <javierpb275@gmail.com>
2022-02-09 18:10:21 +0100

    chaining events

--- lesson6/main.js
+++ lesson6/main.js
@@ -1,17 +1,18 @@
 $(document).ready(() => {
   $(".content-box").click(function () {
-    $(".content-box").animate(
-      {
-        width: "500px",
-        height: "400px",
-      },
-      3000
-    );
-    $(".content-box").animate(
-      {
-        fontSize: "30px",
-      },
-      3000
-    );
+    $(".content-box")
+      .animate(
+        {
+          width: "500px",
+          height: "400px",
+        },
+        3000
+      )
+      .animate(
+        {
+          fontSize: "30px",
+        },
+        3000
+      ).fadeOut();
   });
 });
