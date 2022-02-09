d598b685fa02d64c3d6df5a4c3ab75475c645a23
javierpb275 <javierpb275@gmail.com>
2022-02-09 17:59:55 +0100

    added animations

--- lesson5/main.js
+++ lesson5/main.js
@@ -1,1 +1,18 @@
-
+$(document).ready(() => {
+  $(".content-box").click(function () {
+    $(".content-box").animate(
+      {
+        width: "500px",
+        height: "+=200px",
+      },
+      1000,
+      alert("it works")
+    );
+    $(".content-after").show().animate(
+      {
+        opacity: "1",
+      },
+      3000
+    );
+  });
+});
